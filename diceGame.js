d89d0e55e72307a4289816a972cc8d2f4fefda3e
unknown <Faperez38s@gmail.com>
2019-01-14 13:37:11 -0600

    'diceRoller' function added. Takes input as number of sides for die, then simulates a roll

---
+++ diceGame.js
@@ -0,0 +1,7 @@
+//A game revolving around different dice rolls
+
+//Takes an input as number of sides of a dice and "rolls"
+function diceRoller(numberOfSides){
+let result = Math.floor(Math.random() * numberOfSides)+1;
+}
+
