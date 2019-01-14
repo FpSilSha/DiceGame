064fcdfef4590b4cd57691c240f8efad1b8f186b
unknown <Faperez38s@gmail.com>
2019-01-14 15:01:11 -0600

    Added 'Health Pool' to store possible object/monster health. Added function to pool a value from health pool as a monster/object health. Added monster variable to hold health value.

--- diceGame.js
+++ diceGame.js
@@ -1,7 +1,18 @@
-//A game revolving around different dice rolls
+//A game revolving around different dice rolls.
 
-//Takes an input as number of sides of a dice and "rolls"
+//Takes an input as number of sides of a dice and "rolls".
+let healthPool = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+
 function diceRoller(numberOfSides){
 let result = Math.floor(Math.random() * numberOfSides)+1;
 }
+
+// Function to choose a value from an array at random.
+function randomChoiceFromArray(arrayToRandomlyPickFrom){
+	return arrayToRandomlyPickFrom[Math.floor(Math.random()*arrayToRandomlyPickFrom.length)];
+
+}
+// Health array to pull for random "opponent" health.
+let monster = randomChoiceFromArray(healthPool)
 
