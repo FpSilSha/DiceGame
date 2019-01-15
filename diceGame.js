eca672b0ec489f7ee60a8c6557234b193b12cb46
unknown <Faperez38s@gmail.com>
2019-01-14 23:07:59 -0600

    Cleaned up generateNewMonster function to be very specific and added it as a conditional result of the attackAndHealthCheck function. Added health pools into an array for a later function that can pick pools based on damage level. Renamed damageDelt variable to damageDealingLevel in preparation of a function to upgrade damage power. Added a cash variable in preparation of a money system to buy upgrades with fictional money earned from monster kills.

--- diceGame.js
+++ diceGame.js
@@ -5,10 +5,12 @@
 let healthPoolSmall = [10, 40, 60, 100];
 let healthPoolMedium = [150, 300, 450, 600];
 let healthPoolLarge = [1000, 1500, 2000, 2500];
-let monsterHealth = randomChoiceFromArray(healthPoolSmall)
+let monsterHealth = randomChoiceFromArray(healthPoolSmall);
+let damageVersusMonsterHealth = [healthPoolSmall, healthPoolMedium, healthPoolLarge]
 // Damage Variables
-let damageDealing = 1;
-
+let damageDealingLevel = 1;
+//Misc Variables
+let cash = 0;
 // Functions declared
 // Takes an input as number of sides of a dice and "rolls".
 function diceRoller(numberOfSides){
@@ -20,18 +22,25 @@ function randomChoiceFromArray(arrayToRandomlyPickFrom){
 	return arrayToRandomlyPickFrom[Math.floor(Math.random()*arrayToRandomlyPickFrom.length)];
 
 }
-// Health array to pull for random "opponent" health.
+// Health array to pull for random monster health.
 
 // Function to generate a new monster.
-function generateNewMonster(){
+function generateNewMonster(healthPool){
+	
+		monsterHealth = randomChoiceFromArray(healthPool);
+}		
+	
+
+
+function attackAndHealthCheck(){
+	monsterHealth = monsterHealth - damageDealingLevel;
 	if (monsterHealth <=0){
-		monsterHealth = randomChoiceFromArray(healthPoolSmall);
+
+		generateNewMonster(damageVersusMonsterHealth[0]);
 		alert("Monster defeated! New monster health at " + monsterHealth + "!");
-		
+}
+	else{
+		console.log("The monster is at " + monsterHealth + " hp, keep going!")
 	}
 }
 
-function attack(){
-	monsterHealth = monsterHealth - damageDealing
-	generateNewMonster
-}
