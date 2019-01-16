8e7b720625984310e1042f000508eac7de76b127
unknown <Faperez38s@gmail.com>
2019-01-15 20:53:22 -0600

    added a shopPrice variable and an upgradeDamage function to allow player to increase their weapon strength. Will attach function call to button in future update. Prepared CSS ID's for the current money identifier, the output of the game box, and the main button. Will add value in a future update.

--- diceGame.js
+++ diceGame.js
@@ -6,15 +6,16 @@ let healthPoolSmall = [10, 40, 60, 100];
 let healthPoolMedium = [150, 300, 450, 600];
 let healthPoolLarge = [1000, 1500, 2000, 2500];
 let monsterHealth = randomChoiceFromArray(healthPoolSmall);
-let damageVersusMonsterHealth = [healthPoolSmall, healthPoolMedium, healthPoolLarge]
+let damageVersusMonsterHealth = [healthPoolSmall, healthPoolMedium, healthPoolLarge];
 // Damage Variables
-let damageLevelUpgrades = [1, 5, 10, 25, 50, 75, 100, 150, 200]
+let damageLevelUpgrades = [1, 5, 10, 25, 50, 75, 100, 150, 200];
 let i = 0;
-let damageDealingLevel = damageLevelUpgrades[i];
-let damage = diceRoller(damageDealingLevel)
+//////////let damageDealingLevel = damageLevelUpgrades[i]; Do I need this? Will see if seperately storing this matters later
+let damage = diceRoller(damageLevelUpgrades[i]);
 //Misc Variables
+let shopPrice = [100, 250, 600, 1000, 1450, 2000, 3000, 5000];
 let cash = 0;
-let upgradeCost = 0;
+let upgradeCost = shopPrice[i];
 // Functions declared
 alert("A monster claws it's way towards you with "+ monsterHealth+" hp");
 // Takes an input as number of sides of a dice and "rolls".
@@ -37,13 +38,14 @@ function generateNewMonster(healthPool){
 }		
 	
 
-// Function for health changes. Both deals damage and calls for replacement monster.
+// Function for health changes. Deals damage to monsters.
 
 
 function attack(){
 	monsterHealth -= damage;
 }
 
+// Checks health to report value to user or initialize a new monster.
 function healthCheck(){
 	if (monsterHealth <=0){
 
@@ -52,20 +54,35 @@ function healthCheck(){
 		document.getElementById("outputBox").innerHTML = ("A new monster spawns with " +monsterHealth+" hp!");
 }
 	else{
-		document.getElementById("outputBox").innerHTML = ("The monster is at " + monsterHealth + " hp, keep going!")
+		document.getElementById("outputBox").innerHTML = ("The monster is at " + monsterHealth + " hp, keep going!");
 	}
 
 }
-
+// Function that handles adding money earned from attacking into a capture variable.
 function lootDrop(){
-cash += damage
-	document.getElementById("bank").innerHTML =("Cash Monies value: "+cash+" CM")
+cash += damage;
+	document.getElementById("bank").innerHTML =("Cash Monies value: "+cash+" CM");
 }
+// Function made to coincide with an upgrade button. Gives appropiate responses
+// for upgrade attempts, while also incrementing 'i' under correct conditions.
+function upgradeDamage(){
+if (cash >= upgradeCost && i<=damageLevelUpgrades.length){
+	i++
+	alert("Your power grows...and yet you crave MORE...");
+}
+	else if (cash >= upgradeCost && i === damageLevelUpgrades.length){
+		alert("Your power level is already off the charts! WE CAN'T GO HIGHER!");
+	}
+	else (cash < upgradeCost){
+		alert("Maybe I need to increase the text size if you think you have enough CM");
 
+	}
+
+// The functions that runs the main constraints of the game to actually "play" it.
 function clickActions(){
-	attack()
-	healthCheck()
-	lootDrop()
+	attack();
+	healthCheck();
+	lootDrop();
 	
 }
 
